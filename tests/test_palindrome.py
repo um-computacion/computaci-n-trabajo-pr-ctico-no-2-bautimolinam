84277dc8a3186413ccf5f65ff3a03915f6e9c4c0
bautimolinam <bm.molina@alumno.um.edu.ar>
2025-04-16 09:37:12 -0300

    casos edge

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -4,14 +4,10 @@ from src.palindrome import is_palindrome
 class TestPalindrome(unittest.TestCase):
   
 
-  
-
-    def test_non_palindromes(self):
-        self.assertFalse(is_palindrome("hello"))
-        self.assertFalse(is_palindrome("python"))
-        self.assertFalse(is_palindrome("This is not a palindrome"))
-
-   
+    def test_edge_cases(self):
+        self.assertTrue(is_palindrome(""))
+        self.assertTrue(is_palindrome("a"))
+        self.assertTrue(is_palindrome("A"))
 
 if __name__ == '__main__':
     unittest.main()
